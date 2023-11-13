b5f46a1e26edcb9b04634fd902faf0522eb3f1b4
Andrew Mullen <139258459+andmulLABS01@users.noreply.github.com>
2023-11-13 11:09:48 -0500

    Update jenkins_main.tf

--- jenkins_main.tf
+++ jenkins_main.tf
@@ -8,43 +8,43 @@ provider "aws" {
 
 # Data source to retrieve an existing VPC
 data "aws_vpc" "existing_vpc" {
-  id = "vpc-0c55f19fa002ee274"  # Replace with the ID of your existing VPC
+  id = ""  # Replace with the ID of your existing VPC
 }
 
 # Data source to retrieve an existing subnet
 data "aws_subnet" "existing_subnet1" {
   # Replace with the ID of your existing subnet
-  id = "subnet-0f34dce27312ba3fe"  
+  id = ""  
 }
 
  # Data source to retrieve an existing subnet
  data "aws_subnet" "existing_subnet2" {
    # Replace with the ID of your existing subnet
-   id = "subnet-0bbc2ffd333a2a19d"  
+   id = ""  
  }
 
  # Data source to retrieve an existing subnet
  data "aws_subnet" "existing_subnet3" {
    # Replace with the ID of your existing subnet
-   id = "subnet-0fabec0f207b3527b"  
+   id = ""  
  }
 
 # Data source to retrieve an existing security group
 data "aws_security_group" "existing_sg" {
   # Replace with the name of your existing security group
-  id = "sg-0c3dd472b3163cb37"  
+  id = ""  
 }
 
 # Create the second EC2 instance in the existing subnet
 resource "aws_instance" "instance1" {
   # Replace with your desired AMI ID
-  ami           = "ami-0fc5d935ebf8bc3bc"
+  ami           = "ami-0c"
   instance_type = "t2.micro"
   # Reference the existing subnet
   subnet_id     = data.aws_subnet.existing_subnet1.id  
   security_groups = [data.aws_security_group.existing_sg.id]
   # Replace with your SSH key pair name
-  key_name      = "D6_keypair"
+  key_name      = ""
   # Specify the desired availability zone
   availability_zone = "us-east-1a"
   user_data = "${file("s_jenkins.sh")}"
@@ -57,13 +57,13 @@ resource "aws_instance" "instance1" {
 # Create the second EC2 instance in the existing subnet
 resource "aws_instance" "instance2" {
   # Replace with your desired AMI ID
-  ami           = "ami-0fc5d935ebf8bc3bc"
+  ami           = "ami-0c"
   instance_type = "t2.medium"
   # Reference the existing subnet
   subnet_id     = data.aws_subnet.existing_subnet2.id  
   security_groups = [data.aws_security_group.existing_sg.id]
   # Replace with your SSH key pair name
-  key_name      = "D6_keypair"
+  key_name      = ""
   # Specify the desired availability zone
   availability_zone = "us-east-1b"
   user_data = "${file("s_docker.sh")}"
@@ -76,13 +76,13 @@ resource "aws_instance" "instance2" {
 # Create the second EC2 instance in the existing subnet
 resource "aws_instance" "instance3" {
   # Replace with your desired AMI ID
-  ami           = "ami-0fc5d935ebf8bc3bc"
+  ami           = "ami-0c"
   instance_type = "t2.micro"
   # Reference the existing subnet
   subnet_id     = data.aws_subnet.existing_subnet3.id  
   security_groups = [data.aws_security_group.existing_sg.id]
   # Replace with your SSH key pair name
-  key_name      = "D6_keypair"
+  key_name      = ""
   # Specify the desired availability zone
   availability_zone = "us-east-1c"
   user_data = "${file("s_terraform.sh")}"
